5e3ac73a9cf57deb8a9bfb0514855044eb168ba3
SaadZahem <saadzahem7@gmail.com>
2026-06-18 16:22:55 +0300

    feat(nvim): sile syntax highlighting

--- dot-config/nvim/lua/plugins/misc.lua
+++ dot-config/nvim/lua/plugins/misc.lua
@@ -34,11 +34,15 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = { signs = false },
   },
+  -- {
+  --   -- High-performance color highlighter
+  --   'norcalli/nvim-colorizer.lua',
+  --   config = function()
+  --     require('colorizer').setup()
+  --   end,
+  -- },
   {
-    -- High-performance color highlighter
-    'norcalli/nvim-colorizer.lua',
-    config = function()
-      require('colorizer').setup()
-    end,
+    'sile-typesetter/vim-sile',
+    ft = 'sile',
   },
 }
